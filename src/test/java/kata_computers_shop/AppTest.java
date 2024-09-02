5a889af6a61d0e9fd281f9fa674dcf02286defeb
Pablo-SanJose-Acedo <psanjosea@gmail.com>
2024-09-02 12:29:16 +0200

    first commit

---
+++ src/test/java/kata_computers_shop/AppTest.java
@@ -0,0 +1,18 @@
+package kata_computers_shop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
